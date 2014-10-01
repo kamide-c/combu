958b7600132a3a763cd37c9bd0fbc4de5009dfe5
Cesar Kamide <ckamide@gmail.com>
2014-10-01 16:54:52 -0300

    delete folder android platform

--- platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/Maczaum/Desktop/PW-Consulting-Novelis/Projeto/PW-Consulting/apache-tomcat-7.0.52/webapps/combu/platforms/android/ant-build/AndroidManifest.xml : \
-/Users/Maczaum/Desktop/PW-Consulting-Novelis/Projeto/PW-Consulting/apache-tomcat-7.0.52/webapps/combu/platforms/android/AndroidManifest.xml \
