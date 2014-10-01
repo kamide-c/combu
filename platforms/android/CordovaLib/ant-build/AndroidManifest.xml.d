4e249dd4bff7929121dabaa89783e21ed4b409b1
Cesar Kamide <ckamide@gmail.com>
2014-10-01 16:51:53 -0300

    build android

---
+++ platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/Maczaum/Desktop/PW-Consulting-Novelis/Projeto/PW-Consulting/apache-tomcat-7.0.52/webapps/combu/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
+/Users/Maczaum/Desktop/PW-Consulting-Novelis/Projeto/PW-Consulting/apache-tomcat-7.0.52/webapps/combu/platforms/android/CordovaLib/AndroidManifest.xml \
---
+++ platforms/android/ant-build/HelloCordova-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+/Users/Maczaum/Desktop/PW-Consulting-Novelis/Projeto/PW-Consulting/apache-tomcat-7.0.52/webapps/combu/platforms/android/ant-build/HelloCordova-debug-unaligned.apk : \
+/Users/Maczaum/Desktop/PW-Consulting-Novelis/Projeto/PW-Consulting/apache-tomcat-7.0.52/webapps/combu/platforms/android/ant-build/HelloCordova.ap_ \
+/Users/Maczaum/Desktop/PW-Consulting-Novelis/Projeto/PW-Consulting/apache-tomcat-7.0.52/webapps/combu/platforms/android/ant-build/classes.dex \
+/Users/Maczaum/Desktop/PW-Consulting-Novelis/Projeto/PW-Consulting/apache-tomcat-7.0.52/webapps/combu/platforms/android/CordovaLib/ant-build/classes.jar \
